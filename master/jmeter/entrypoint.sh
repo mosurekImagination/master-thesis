7b59b86cf40385f383f40624eff756fafcc2f84b
Tomasz Mosur <tomasz@mosur.net>
2020-05-04 15:58:56 +0200

    DAY 3 - Perfmon, working

--- master/jmeter/entrypoint.sh
+++ master/jmeter/entrypoint.sh
@@ -7,39 +7,41 @@ echo "Europe/Warsaw" >  /etc/timezone
 apk del tzdata
 
 echo "WAITING 100sec to systems up"
-# sleep 3
+sleep 3
 echo "3sec remaining"
-# sleep 3
+sleep 3
 
 dateTime=$(date +'%Y-%m-%d-%H.%M')
 echo $dateTime
 echo "### $dateTime STARTING 1 LOAD TESTING OF $FIRST_WORKER_HOST:$FIRST_WORKER_PORT ###"
 resultsFolder=./test-results/$dateTime
-mkdir ./temp-test-results/
+tempResultsFolder=./temp-test-results #perfmon report is hardcoded to /temp-test-results/perfmon.csv, due to not resolving variables
 mkdir $resultsFolder
 
-./bin/jmeter -Jhostadress=$FIRST_WORKER_HOST -Jport=$FIRST_WORKER_PORT  -n -t ./test-scenarios/simple.jmx -l ./temp-test-results/results.log -e -o ./temp-test-results/raport/
-mv ./temp-test-results/ $resultsFolder/1-$FIRST_WORKER_HOST/
+mkdir $tempResultsFolder/
+./bin/jmeter -JforcePerfmonFile=true -Jhostadress=$FIRST_WORKER_HOST -Jhostport=$FIRST_WORKER_PORT  -n -t ./test-scenarios/simple.jmx -l $tempResultsFolder/results.log -e -o $tempResultsFolder/raport/ -JperfmonReportPath=$tempResultsFolder/perfmon.csv -JaggregateReportPath=$tempResultsFolder/aggregateReport.csv -JsummaryReportPath=$tempResultsFolder/summaryReport.csv -JgraphReportPath=$tempResultsFolder/graphReport.csv -JtableReportPath=$tempResultsFolder/graphReport.csv
+mv $tempResultsFolder/ $resultsFolder/1-$FIRST_WORKER_HOST/
 
 echo "WAITING 100sec to systems up"
 sleep 3
 echo "3sec remaining"
 sleep 3
 
-mkdir ./temp-test-results/
+mkdir $tempResultsFolder/
 echo "### STARTING 2 LOAD TESTING OF $SECOND_WORKER_HOST:$SECOND_WORKER_PORT###"
-./bin/jmeter -Jhostadress=$SECOND_WORKER_HOST -Jport=$SECOND_WORKER_PORT  -n -t ./test-scenarios/simple.jmx -l ./temp-test-results/results.log -e -o ./temp-test-results/raport/
-mv ./temp-test-results/ $resultsFolder/2-$SECOND_WORKER_HOST/
+./bin/jmeter -Jhostadress=$SECOND_WORKER_HOST -Jhostport=$SECOND_WORKER_PORT  -n -t ./test-scenarios/simple.jmx -l $tempResultsFolder/results.log -e -o $tempResultsFolder/raport/ -JperfmonReportPath=$tempResultsFolder/perfmon.csv -JaggregateReportPath=$tempResultsFolder/aggregateReport.csv -JsummaryReportPath=$tempResultsFolder/summaryReport.csv -JgraphReportPath=$tempResultsFolder/graphReport.csv -JtableReportPath=$tempResultsFolder/graphReport.csv
+mv $tempResultsFolder/ $resultsFolder/2-$SECOND_WORKER_HOST/
+
 
 echo "WAITING 100sec to systems up"
 sleep 3
 echo "3sec remaining"
 sleep 3
 
-mkdir ./temp-test-results/
+mkdir $tempResultsFolder/
 echo "### STARTING 3 LOAD TESTING OF $THIRD_WORKER_HOST:$THIRD_WORKER_PORT###"
-./bin/jmeter -Jhostadress=$THIRD_WORKER_HOST -Jport=$THIRD_WORKER_PORT  -n -t ./test-scenarios/simple.jmx -l ./temp-test-results/results.log -e -o ./temp-test-results/raport/
-mv ./temp-test-results/ $resultsFolder/3-$THIRD_WORKER_HOST/
+./bin/jmeter -Jhostadress=$THIRD_WORKER_HOST -Jhostport=$THIRD_WORKER_PORT  -n -t ./test-scenarios/simple.jmx -l $tempResultsFolder/results.log -e -o $tempResultsFolder/raport/ -JperfmonReportPath=$tempResultsFolder/perfmon.csv -JaggregateReportPath=$tempResultsFolder/aggregateReport.csv -JsummaryReportPath=$tempResultsFolder/summaryReport.csv -JgraphReportPath=$tempResultsFolder/graphReport.csv -JtableReportPath=$tempResultsFolder/graphReport.csv
+mv $tempResultsFolder/ $resultsFolder/3-$THIRD_WORKER_HOST/
 
 echo "### LOAD TESTING FINISHED ###"
 
