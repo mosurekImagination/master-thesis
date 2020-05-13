4cd53a268bfbc3505ba609113608c91595e3467e
Tomasz Mosur <tomasz@mosur.net>
2020-05-13 21:23:58 +0200

    mongo number id, default pool boot, test changes

--- workers/webflux-mongodb/src/main/java/net/mosur/mosurthesisworkerwebflux/entity/ThesisEntity.java
+++ workers/webflux-mongodb/src/main/java/net/mosur/mosurthesisworkerwebflux/entity/ThesisEntity.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 @NoArgsConstructor
@@ -17,7 +18,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class ThesisEntity {
 
     @Id
-    ObjectId id;
+    @Field("_id")
+    String id;
     Long number;
     String name;
 }
